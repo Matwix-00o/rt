df4a09dcf589450af60b9680d60d52f62ddc3392
Kirito <urijarom@gmail.com>
2025-03-22 14:59:57 +0100

    Create home.js

---
+++ home.js
@@ -0,0 +1,14 @@
+function win24() {
+	window.location.href = "win24.html"
+}
+
+function früh24() {
+	window.location.href = "früh24.html"
+}
+
+function som24() {
+	window.location.href = "som24.html"
+}
+function herb24() {
+	window.location.href = "herb24.html"
+}
