dd54bf550ef7c723df9496ca2dfd96d403946053
Kirito <urijarom@gmail.com>
2025-03-24 19:49:11 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,46 @@
- function _0x1899(_0x4d2f0f,_0x15685a){const _0x5a702c=_0x5a70();return _0x1899=function(_0x189944,_0x224b82){_0x189944=_0x189944-0x88;let _0x423944=_0x5a702c[_0x189944];return _0x423944;},_0x1899(_0x4d2f0f,_0x15685a);}const _0x386c0c=_0x1899;function _0x5a70(){const _0x43f2a1=['1426592OqZMOs','some','752KDvFKN','3qKjmuv','getElementById','romtromps','location','meldung','shiftKey','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20style=\x22text-align:\x20center;\x20margin-top:\x2050px;\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<h1>Loging\x20into\x20Cloud</h1>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<img\x20src=\x22https://i.gifer.com/7gQj.gif\x22\x20alt=\x22Ladeanimation\x22\x20style=\x22width:\x20200px;\x22/>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20','❌\x20Falsches\x20Passwort!','9818ZejyJa','F12','10VwjCqK','value','ctrlKey','2614899TUUwRk','28754401RLodxZ','contextmenu','addEventListener','home.html','shift','58505TkBsCf','6435517BHljMm','12DTmonM','87ZwDbgy','innerHTML','402NVxqeD','body','Kein\x20cheaten!!!','99531DmEbOJ','preventDefault','key'];_0x5a70=function(){return _0x43f2a1;};return _0x5a70();}(function(_0x3d275a,_0x3b28ec){const _0x398a34=_0x1899,_0x239229=_0x3d275a();while(!![]){try{const _0x5e216b=parseInt(_0x398a34(0x8a))/0x1*(parseInt(_0x398a34(0x9d))/0x2)+parseInt(_0x398a34(0x95))/0x3*(-parseInt(_0x398a34(0x92))/0x4)+-parseInt(_0x398a34(0xa8))/0x5*(-parseInt(_0x398a34(0x8c))/0x6)+parseInt(_0x398a34(0xa2))/0x7+parseInt(_0x398a34(0x94))/0x8*(parseInt(_0x398a34(0x8f))/0x9)+parseInt(_0x398a34(0x9f))/0xa*(parseInt(_0x398a34(0x88))/0xb)+-parseInt(_0x398a34(0x89))/0xc*(parseInt(_0x398a34(0xa3))/0xd);if(_0x5e216b===_0x3b28ec)break;else _0x239229['push'](_0x239229['shift']());}catch(_0x54aa93){_0x239229['push'](_0x239229['shift']());}}}(_0x5a70,0x9c6a3));function checkPasswort(){const _0x15db2d=_0x1899;let _0x388fee=document[_0x15db2d(0x96)]('passwortEingabe')[_0x15db2d(0xa0)],_0xa1d1ae=_0x15db2d(0x97);_0x388fee===_0xa1d1ae?(document[_0x15db2d(0x8d)][_0x15db2d(0x8b)]=_0x15db2d(0x9b),setTimeout(()=>{const _0x511087=_0x15db2d;window[_0x511087(0x98)]['href']=_0x511087(0xa6);},0xbb8)):document[_0x15db2d(0x96)](_0x15db2d(0x99))[_0x15db2d(0x8b)]=_0x15db2d(0x9c);}document['addEventListener']('keydown',function(_0x1d69f1){const _0x517d86=_0x1899,_0x4983d9=[{'ctrl':!![],'shift':![],'key':'u'},{'ctrl':!![],'shift':!![],'key':'i'},{'ctrl':!![],'shift':!![],'key':'j'},{'ctrl':!![],'shift':!![],'key':'c'}];(_0x1d69f1[_0x517d86(0x91)]===_0x517d86(0x9e)||_0x4983d9[_0x517d86(0x93)](_0x447605=>_0x1d69f1[_0x517d86(0xa1)]===_0x447605['ctrl']&&_0x1d69f1[_0x517d86(0x9a)]===_0x447605[_0x517d86(0xa7)]&&_0x1d69f1[_0x517d86(0x91)]['toLowerCase']()===_0x447605[_0x517d86(0x91)]))&&(_0x1d69f1[_0x517d86(0x90)](),alert(_0x517d86(0x8e)));}),document[_0x386c0c(0xa5)](_0x386c0c(0xa4),function(_0x70cdf8){const _0x4c82a8=_0x386c0c;_0x70cdf8[_0x4c82a8(0x90)](),alert(_0x4c82a8(0x8e));});
+// Passwortüberprüfung
+function checkPasswort() {
+            let password = document.getElementById("passwortEingabe").value;
+            const correctPassword = "romtromps4ever!"; // Beispiel-Passwort
+            if (password === correctPassword) {
+                // Weiterleitung zur geheimen Seite mit einem geheimen Schlüssel
+                setTimeout(() => {
+                    window.location.href = "home.html?key=romtromps!"; // Weiterleitung mit geheimem Schlüssel
+                }, 500);
+            } else {
+                document.getElementById("meldung").innerHTML = "❌ Falsches Passwort!";
+            }
+        }
+
+ 
+
+// Prüfen, ob der Nutzer bereits eingeloggt ist
+function checkLogin() {
+    if (getCookie("loggedIn") === "true") {
+        window.location.href = "home.html"; // Falls schon eingeloggt, weiterleiten
+    }
+}
+
+// Bei Laden der Seite Login-Status prüfen
+checkLogin();
+
+// Blockiert F12 und Entwickler-Tools
+document.addEventListener("keydown", function (event) {
+    let forbiddenKeys = [
+        { ctrl: true, shift: false, key: "u" },
+        { ctrl: true, shift: true, key: "i" },
+        { ctrl: true, shift: true, key: "j" },
+        { ctrl: true, shift: true, key: "c" }
+    ];
+    
+    if (event.key === "F12" || forbiddenKeys.some(k => event.ctrlKey === k.ctrl && event.shiftKey === k.shift && event.key.toLowerCase() === k.key)) {
+        event.preventDefault();
+        alert("Kein Cheaten!!!");
+    }
+});
+
+// Blockiert Rechtsklick
+document.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+    alert("Kein Cheaten!!!");
+});
